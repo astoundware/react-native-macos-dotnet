5d2c8a39a40140ef32de5de0a30817841f22e06a
Kenny McClive <kenny.mcclive@me.com>
2022-04-21 16:35:00 -0600

    Created bindings library project.

---
+++ dotnet/Astound.ReactNative.macOS/Astound.ReactNative.macOS.Bindings/ApiDefinition.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using ObjCRuntime;
+
+namespace Astound.ReactNative.macOS.Bindings
+{
+    // @interface RCTBridge : NSObject <RCTInvalidating>
+    [BaseType(typeof(NSObject))]
+    interface RCTBridge
+    {
+        // -(instancetype)initWithDelegate:(id<RCTBridgeDelegate>)delegate launchOptions:(NSDictionary *)launchOptions;
+        [Export("initWithDelegate:launchOptions:")]
+        IntPtr Constructor(NSObject @delegate, [NullAllowed] NSDictionary launchOptions);
+    }
+
+    // @interface RCTBundleURLProvider : NSObject
+    [BaseType(typeof(NSObject))]
+    interface RCTBundleURLProvider
+    {
+        // +(instancetype)sharedSettings;
+        [Static]
+        [Export("sharedSettings")]
+        RCTBundleURLProvider SharedSettings();
+
+        // -(NSURL *)jsBundleURLForBundleRoot:(NSString *)bundleRoot fallbackResource:(NSString *)resourceName;
+        [Export("jsBundleURLForBundleRoot:fallbackResource:")]
+        NSUrl JsBundleURLForBundleRoot(string bundleRoot, [NullAllowed] string resourceName);
+    }
+
+    // @interface RCTRootView : RCTUIView
+    [BaseType(typeof(RCTUIView))]
+    interface RCTRootView
+    {
+        // -(instancetype)initWithBundleURL:(NSURL *)bundleURL moduleName:(NSString *)moduleName initialProperties:(NSDictionary *)initialProperties launchOptions:(NSDictionary *)launchOptions;
+        [Export("initWithBundleURL:moduleName:initialProperties:launchOptions:")]
+        IntPtr Constructor(NSUrl bundleURL, string moduleName, [NullAllowed] NSDictionary initialProperties, [NullAllowed] NSDictionary launchOptions);
+
+        // - (instancetype)initWithBridge:(RCTBridge *)bridge moduleName:(NSString*) moduleName initialProperties:(NSDictionary*) initialProperties
+        [Export("initWithBridge:moduleName:initialProperties:")]
+        IntPtr Constructor(RCTBridge bridge, string moduleName, [NullAllowed] NSDictionary initialProperties);
+    }
+
+    // @interface RCTUIView : NSView
+    [BaseType(typeof(NSView))]
+    interface RCTUIView
+    {
+        // -(void)setBackgroundColor:(NSColor *)backgroundColor
+        [Export("setBackgroundColor:")]
+        void SetBackgroundColor(NSColor backgroundColor);
+    }
+
+    // @protocol RCTBridgeModule <NSObject>
+    [Protocol, Model]
+    [BaseType(typeof(NSObject))]
+    interface RCTBridgeModule
+    {
+
+    }
+
+    // @protocol RCTBridgeDelegate <NSObject>
+    [Protocol, Model]
+    [BaseType(typeof(NSObject))]
+    interface RCTBridgeDelegate
+    {
+        // -(NSURL *)sourceURLForBridge:(RCTBridge *)bridge;
+        [Export("sourceURLForBridge:")]
+        [Abstract]
+        NSUrl SourceURLForBridge(RCTBridge bridge);
+    }
+
+    // @interface RCTEventEmitter : NSObject <RCTBridgeModule, RCTJSInvokerModule, RCTInvalidating>
+    [BaseType(typeof(RCTBridgeModule))]
+    interface RCTEventEmitter
+    {
+        // -(id)supportedEvents;
+        [Export("supportedEvents")]
+        [Abstract]
+        string[] SupportedEvents { get; }
+
+        // -(void)sendEventWithName:(NSString *_Nullable)name body:(id _Nullable )body;
+        [Export("sendEventWithName:body:")]
+        void SendEventWithName(string name, [NullAllowed] NSObject body);
+
+        // -(void)startObserving;
+        [Export("startObserving")]
+        void StartObserving();
+
+        // -(void)stopObserving;
+        [Export("stopObserving")]
+        void StopObserving();
+    }
+}
+
