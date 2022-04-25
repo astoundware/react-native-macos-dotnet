34d1bd7a9aa869d7d0a45a734afbaff5fc7f334c
Kenny McClive <kmcclive@captechconsulting.com>
2022-04-25 11:18:41 -0600

    Fixed reference problems with nuget package.

---
+++ dotnet/Astound.ReactNative.macOS.Bindings/libReactNative-macOS.linkwith.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using ObjCRuntime;
+
+[assembly: LinkWith("libReactNative-macOS.a", LinkTarget = LinkTarget.Arm64 | LinkTarget.x86_64, LinkerFlags = "-lstdc++", SmartLink = true, ForceLoad = true)] 
