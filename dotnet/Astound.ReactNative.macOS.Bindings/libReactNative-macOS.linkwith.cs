7f649772d1d1074f272426c3f7aacd5034904e69
Kenny McClive <kmcclive@captechconsulting.com>
2022-04-25 15:36:34 -0600

    Reverted back to using NativeReference.

--- dotnet/Astound.ReactNative.macOS.Bindings/libReactNative-macOS.linkwith.cs
+++
@@ -1,4 +0,0 @@
-﻿using System;
-using ObjCRuntime;
-
-[assembly: LinkWith("libReactNative-macOS.a", LinkTarget = LinkTarget.Arm64 | LinkTarget.x86_64, LinkerFlags = "-lstdc++", SmartLink = true, ForceLoad = true)] 
