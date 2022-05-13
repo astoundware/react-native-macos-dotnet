2c5cdc1f84f3d084a703502c49045730b8f9df0e
Kenny McClive <kmcclive@captechconsulting.com>
2022-05-13 13:57:39 -0600

    Used new packages from react-native-dotnet project.

--- dotnet/Astound.ReactNative.macOS.Extensions/ReactAppDelegate.cs
+++ dotnet/Astound.ReactNative.macOS.Extensions/ReactAppDelegate.cs
@@ -1,5 +1,8 @@
-﻿using AppKit;
+﻿using System.Reflection;
+using AppKit;
 using Astound.ReactNative.macOS.Bindings;
+using Astound.ReactNative.Modules;
+using Astound.ReactNative.Modules.ObjC;
 using Foundation;
 
 namespace Astound.ReactNative.macOS.Extensions
@@ -10,12 +13,21 @@ namespace Astound.ReactNative.macOS.Extensions
 
         protected virtual string BundleRoot => "index";
         protected virtual string FallbackResourceName => "main";
+        protected virtual IReactModuleRegistry ModuleRegistry { get; } =
+            new ReactModuleRegistry(ReactFunctionsWrapper.Self);
 
         public override void AwakeFromNib()
         {
             base.AwakeFromNib();
 
+            RegisterModules(ModuleRegistry);
+
             Bridge = new RCTBridge(this, null);
+        }
+
+        public virtual void RegisterModules(IReactModuleRegistry registry)
+        {
+            registry.Register(Assembly.GetEntryAssembly());
         }
 
         public virtual NSUrl SourceURLForBridge(RCTBridge bridge)
---
+++ dotnet/Astound.ReactNative.macOS.Extensions/ReactFunctionsWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Astound.ReactNative.Bindings;
+using Astound.ReactNative.Modules.ObjC;
+using ObjCRuntime;
+
+namespace Astound.ReactNative.macOS.Extensions
+{
+	class ReactFunctionsWrapper : IReactFunctions
+	{
+		public static ReactFunctionsWrapper Self { get; } = new ReactFunctionsWrapper();
+
+		private ReactFunctionsWrapper()
+		{
+
+		}
+
+		public void RegisterModule(string moduleName) => RCTFunctions.RCTRegisterModule(Class.GetHandle(moduleName));
+	}
+}
+
