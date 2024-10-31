f68d48ede96e1479e9d001240cf324ee472f7508
Kenny McClive <kenny.mcclive@me.com>
2024-10-30 19:51:04 -0500

    Reconfigured bundler target.

--- dotnet/Astound.ReactNative.macOS.Bindings/scripts/bundle.sh
+++ dotnet/Astound.ReactNative.macOS.Bindings/scripts/bundle.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+msg_prefix="React Bundle:"
+lock_file=".lock"
+
+if [[ -f "$lock_file" ]]; then
+  echo "$msg_prefix lock file found"
+  react_bundle_count=$(cat "$lock_file")
+fi
+
+if [[ $react_bundle_count =~ \\d+ ]]; then
+  echo "$msg_prefix lock file is invalid"
+  react_bundle_count=0
+fi
+
+react_bundle_count=$((react_bundle_count + 1))
+
+if (( react_bundle_count > 3 )); then
+  echo "$msg_prefix resetting lock"
+  rm -f -- "$lock_file"
+  react_bundle_count=1
+fi
+
+echo "$react_bundle_count" > ".lock"
+
+if (( react_bundle_count > 1 )); then
+  echo "$msg_prefix attempt #$react_bundle_count; skipping"
+  exit 0
+fi
+
 projectPath=$(pwd)
 scriptFile=$(readlink -f "$0")
 scriptPath=$(dirname "$scriptFile")
@@ -10,6 +38,6 @@ export NODE_BINARY=node
 export CONFIGURATION=Release
 export PLATFORM_NAME=macosx
 export CONFIGURATION_BUILD_DIR=$(pwd)
-export UNLOCALIZED_RESOURCES_FOLDER_PATH=${projectPath/$CONFIGURATION_BUILD_DIR\//}/Resources
+export UNLOCALIZED_RESOURCES_FOLDER_PATH=${projectPath/$CONFIGURATION_BUILD_DIR\//}
 
 ./node_modules/react-native-macos/scripts/react-native-xcode.sh
